b02bdfd7ec1f39cec58e8b1e768ec1d47e182ced
Mark Tang <thippo@163.com>
2026-03-31 15:17:16 +0800

    260331

--- overseas/data.js
+++ overseas/data.js
@@ -1,10 +1,10 @@
     const CONFIG = {
         holdings: {
             GBP: { amount: 518.04, name: '英镑', flag: '🇬🇧' },
-            USD: { amount: 12128.63, name: '美元', flag: '🇺🇸' },
-            SGD: { amount: 13149.85, name: '新加坡元', flag: '🇸🇬' }
+            USD: { amount: 11732.48, name: '美元', flag: '🇺🇸' },
+            SGD: { amount: 13164.51, name: '新加坡元', flag: '🇸🇬' }
         },
-        date: "2026-01-31",
+        date: "2026-03-31",
         targetEUR: 500000,
         currencies: ['GBP', 'USD', 'SGD'],
         baseTargets: ['CNY', 'EUR'],
