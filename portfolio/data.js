2a644af7a71027e859f66c7814ca2d4ba191ee61
Mark Tang <thippo@163.com>
2026-06-18 08:33:51 +0800

    a

--- portfolio/data.js
+++ portfolio/data.js
@@ -1,13 +1,13 @@
 let portfolioData = [
-    { ticker: 'C', name: 'CITIGROUP INC', allocation: 2.11 },
-    { ticker: 'CME', name: 'CME GROUP INC CLASS CLASS A', allocation: 16.62 },
+    { ticker: 'C', name: 'CITIGROUP INC', allocation: 2.41 },
+    { ticker: 'CME', name: 'CME GROUP INC CLASS CLASS A', allocation: 14.76 },
     { ticker: 'GOOGL', name: 'ALPHABET INC CLASS A', allocation: 3.05 },
-    { ticker: 'HSAI', name: 'HESAI GROUP FUNSPONSORED ADR 1 ADR REPS 1 ORD SHS', allocation: 1.84 },
-    { ticker: 'NVDA', name: 'NVIDIA CORP', allocation: 16.69 },
-    { ticker: 'VIK', name: 'VIKING HLDGS LTD F', allocation: 2.0 },
-    { ticker: 'TLT', name: 'ISHARES 20+ YEAR TREASURY BOND ETF', allocation: 19.8 },
-    { ticker: 'BIL', name: 'ST STERT SPDR BLMBG 1 3 MN T BL ETF', allocation: 37.8 },
-    { ticker: 'CASH', name: 'Cash & Money Market', allocation: 0.09 }
+    { ticker: 'NVDA', name: 'NVIDIA CORP', allocation: 17.2 },
+    { ticker: 'SPCX', name: 'SPACE EX TECH SPACEX A', allocation: 1.63 },
+    { ticker: 'VIK', name: 'VIKING HLDGS LTD F', allocation: 2.38 },
+    { ticker: 'TLT', name: 'ISHARES 20+ YEAR TREASURY BOND ETF', allocation: 20.15 },
+    { ticker: 'BIL', name: 'ST STERT SPDR BLMBG 1 3 MN T BL ETF', allocation: 38.15 },
+    { ticker: 'CASH', name: 'Cash & Money Market', allocation: 0.28 }
 ];
-const period = "2024/05/14 - 2026/03/30";
-const returnRate = "+11.12%";
+const period = "2024/05/14 - 2026/06/16";
+const returnRate = "+12.77%";
